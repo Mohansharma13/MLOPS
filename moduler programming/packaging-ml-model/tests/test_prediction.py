b24fbaa55e974f3a486522181bb3ead344e6dec0
Mohansharma13 <msharma13613@gmail.com>
2024-07-18 01:11:22 +0530

    test.py

--- moduler programming/packaging-ml-model/tests/test_prediction.py
+++ moduler programming/packaging-ml-model/tests/test_prediction.py
@@ -1,17 +1,20 @@
-# we are going to folow pytest module documentaion for this
-# https://docs.pytest.org/en/8.2.x/
-
-import pytest 
+import pytest
 from prediction_model.config import config
 from prediction_model.processing.data_handling import load_dataset
 from prediction_model.predict import genrate_prediction
 
-
-# fixture in pytest
-# fixture are the function that run before execution of each test function
-
+@pytest.fixture
 def single_prediction():
-    test_dataset=load_dataset(config.TEST_FILE)
-    single_row=test_dataset[:1]
-    result= genrate_prediction(single_row)
-    return(result)
+    test_dataset = load_dataset(config.TEST_FILE)
+    single_row = test_dataset[:1]
+    result = genrate_prediction(single_row)
+    return result
+
+def test_single_pred_not_none(single_prediction):
+    assert single_prediction is not None
+
+def test_single_pred_str_type(single_prediction):
+    assert isinstance(single_prediction.get('predictions')[0], str)
+
+def test_single_pred_validate(single_prediction):
+    assert single_prediction.get('predictions')[0] == 'Y'
