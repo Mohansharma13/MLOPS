63e565197fe829469c343b06403eefc78cef98c6
Mohansharma13 <msharma13613@gmail.com>
2024-08-02 17:54:10 +0530

    docker file

--- Docker/flaskapp/app.py
+++ Docker/flaskapp/app.py
@@ -1,4 +1,21 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
+import random 
+import os
 
 app = Flask(__name__)
 
+images = [
+    "https://raw.githubusercontent.com/manifoldailearning/mlops-with-aws-datascientists/main/Section-11-Docker/images/image1.gif",
+    "https://raw.githubusercontent.com/manifoldailearning/mlops-with-aws-datascientists/main/Section-11-Docker/images/image2.gif",
+    "https://raw.githubusercontent.com/manifoldailearning/mlops-with-aws-datascientists/main/Section-11-Docker/images/image3.gif",
+    "https://raw.githubusercontent.com/manifoldailearning/mlops-with-aws-datascientists/main/Section-11-Docker/images/image4.gif",
+    "https://raw.githubusercontent.com/manifoldailearning/mlops-with-aws-datascientists/main/Section-11-Docker/images/image5.gif"
+]
+
+@app.route("/")
+def index():
+    src = random.choice(images)
+    return render_template("index.html", url=src)
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=int(os.environ.get("PORT", 5000)))
