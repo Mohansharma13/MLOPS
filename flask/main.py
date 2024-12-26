eb4d30fdd0ce451fb7f366424be76568bfa9cf42
Mohan Sharma <msharma13613@gmail.com>
2024-12-27 00:16:05 +0530

    route

--- flask/main.py
+++ flask/main.py
@@ -6,3 +6,10 @@ app=Flask(__name__)
 
 def hello():
     return "<h1>welcome to the world of flask </h1>"
+
+@app.route('/welcome')
+def welcome():
+    return "<h1> welcome to the home page </h1>"
+
+if __name__ == "__main__":
+    app.run()
